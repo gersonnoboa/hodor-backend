e510ceb68283ca7432ccb441e14e7002f19a0698
Gerson Noboa <gerson.noboa@proekspert.ee>
2019-03-03 16:27:49 +0200

    Password is hashed before saving

--- src/data/schemas/user.js
+++ src/data/schemas/user.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   name: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true
+  },
   password: String,
   group: {
     type: mongoose.Schema.Types.ObjectId,
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
+const bcrypt = require("bcrypt");
 
 const User = require("../data/schemas/user");
 
@@ -9,10 +10,21 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
-  const user = new User({
+  try {
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res.status(400).send("User already registered");
+    }
+  } catch(error) {
+    return res.status(400).send("Error saving user.");
+  } 
+  
+  const password = await hashedPassword(req.body.password);
+
+  user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: password,
     group: req.params.group
   });
 
@@ -23,5 +35,11 @@ router.post("/", async (req, res) => {
     res.status(400).send(error);
   }
 });
+
+async function hashedPassword(password) {
+  const salt = await bcrypt.genSalt(10);
+  const hashedPw = await bcrypt.hash(password, salt);
+  return hashedPw;
+}
 
 module.exports = router;
